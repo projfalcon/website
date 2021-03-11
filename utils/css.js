d90e21f00cc30ac6934e2c55aaf860166d62854f
Fábio Ferreira <ffcfpten@gmail.com>
2021-03-11 21:05:32 +0000

    refactor: get Y-axis translation value

--- utils/css.js
+++ utils/css.js
@@ -1,12 +1,26 @@
 export const getTranslateY = (transform) => {
-  // find translateY
+  // Find translateY
   const match = transform.match('translateY')
-  if (!match) return
+  if (!match) {
+    if (!transform.includes('translate')) return
 
-  // get value
-  let str = transform.substr(match.index)
-  str = str.replace('translateY', '')
-  str = str.replace(/\(|\)|%/g, '')
+    let str = transform.replace('translate', '')
+    str = str.replace('(', '').replace(')', '')
+    str = str.substring(str.indexOf(',') + 1)
+    str = str.replace('%', '')
+      .replace('px', '')
+      .replace('rem', '')
+      .replace('re', '')
+      .replace('pt', '')
+      .trim()
 
-  return Number(str)
+    return Number(str)
+  } else {
+    // Get value
+    let str = transform.substr(match.index)
+    str = str.replace('translateY', '')
+    str = str.replace(/\(|\)|%/g, '')
+
+    return Number(str)
+  }
 }

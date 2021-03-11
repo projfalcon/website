c2e36bf4fe8d5b50fe4010fe1b229eb704aefd9c
Fábio Ferreira <ffcfpten@gmail.com>
2021-03-11 03:42:03 +0000

    chores: update manifest filename

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'manifest', href: '/manifest.json' },
       { rel: 'mask-icon', color: '#2d7ff9', href: '/safari-pinned-tab.svg' }
     ]
   },
